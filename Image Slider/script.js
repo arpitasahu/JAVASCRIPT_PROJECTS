f3cc035f97d7979f2aaf41e403b5aaf0cfe861d3
Arpita <arpitasahu299@gmail.com>
2021-01-03 14:55:40 +0530

    script halfly done

---
+++ Image Slider/script.js
@@ -0,0 +1,34 @@
+const slideImage = document.querySelectorAll(".slide-image");
+const slidesContainer = document.querySelector(".slides-container");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const navigationDots = document.querySelector(".navigation-dots");
+
+let numberOfImages = slideImage.length;
+
+//slider setup
+function init() {
+  /*   
+    slideImage[0] = 0
+    slideImage[1] = 100%
+    slideImage[2] = 200%
+    */
+
+  slideImage.forEach((img, i) => {
+    img.style.left = i * 100 + "%";
+  });
+
+  slideImage[0].classList.add("active");
+}
+init();
+
+//Create navigation dots
+function createNavigationDots (){
+  for(let i =0 ; i< numberOfImages; i++){
+    const dot = document.createElement('div');
+    dot.classList.add('single-dot');
+    navigationDots.appendChild(dot);
+  }
+}
+
+
